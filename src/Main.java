75fbd49daad80bbd7d89c83b43d9e8d78415c0b1
UncleFox <118179582+UncleRedHead@users.noreply.github.com>
2023-10-28 09:56:09 +0200

    zad 3.

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,12 @@ class Hello
 {
     public  static void main(String[ ] args)
     {
-        System.out.println("Hello World!");
+        System.out.println("Hello World");
+        System.out.println("Imie: Dariusz");
+        System.out.println("Nazwisko: Nowak");
+        System.out.println("Wiek: 23");
+        System.out.println("Kierunek: Psychologia");
+        System.out.println("Semestr: 4");
+        System.out.println("Uczelnia: UPH Siedlce");
     }
 }
